deccb9155e5feeafaf66aae56c26a839ff8c4d36
Mbilal120 <muhammadbilal123090@gmail.com>
2024-08-05 22:39:13 +0500

    Slug Isue

--- app/dashboard/content/[slug]/page.tsx
+++ app/dashboard/content/[slug]/page.tsx
@@ -12,6 +12,7 @@ import { db } from '@/utlis/db'
 import { AIOutput } from '@/utlis/schema'
 import { useUser } from '@clerk/nextjs'
 import moment from 'moment';
+
 interface PROPS {
     params: {
         'slug': string
@@ -19,53 +20,53 @@ interface PROPS {
 }
 
 const Page = (props: PROPS) => {
-    const [loading,setLoading] = useState<boolean>(false)
-    const [aiOutput,setAIoutput] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+    const [aiOutput, setAIOutput] = useState<string>("")
     const selectTemplate: TEMPLATE | undefined = Templates?.find((item) => item.slug === props.params['slug'])
-    const {user} = useUser();
+    const { user } = useUser();
 
-    const GenerateAIContent = async (formData:any) => {
+    const GenerateAIContent = async (formData: any) => {
         setLoading(true)
         const SelectPrompt = selectTemplate?.aiPrompt;
 
-        const FinalAiPrompt = JSON.stringify(formData)+", "+SelectPrompt;
+        const FinalAiPrompt = JSON.stringify(formData) + ", " + SelectPrompt;
 
         const result = await chatSession.sendMessage(FinalAiPrompt)
-        setAIoutput(result?.response.text());
-        await SaveInDb(JSON.stringify(formData),selectTemplate?.slug,result?.response.text());
+        const responseText = await result?.response.text();
+        setAIOutput(responseText);
+        await SaveInDb(JSON.stringify(formData), selectTemplate?.slug, responseText);
         setLoading(false)
     }
-    
-    const SaveInDb = async (formData:any,slug:any,aiResp:string) =>{
 
-        console.log(aiResp)
+    const SaveInDb = async (formData: string, slug: string | undefined, aiResp: string) => {
+       
         const result = await db.insert(AIOutput).values({
-            formData:formData,
-            slug:slug,
-            aiResponse:aiResp,
-            createdBy:user?.primaryEmailAddress?.emailAddress,
-            createdAt:moment().format('DD/MM/YYYY')
+            formData: formData,
+            slug: slug || '',
+            aiResponse: aiResp,
+            createdBy: user?.primaryEmailAddress?.emailAddress || '',
+            createdAt: moment().format('DD/MM/YYYY')
         })
-
     }
-  return (
-    <div>
-        <Link href='/dashboard'>
-        <Button className='m-4 flex'> <ArrowLeft className='w-5 h-5 ml-1' /> Back</Button>
-        </Link>
-  
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5'>
-        
-        {/* Form section  */}
-        <FormSection selectedTemplates={selectTemplate} userFormInput={(data:any) => GenerateAIContent(data)} loading={loading}  />
 
-        {/* outputsection  */}
-        <div className='col-span-2'>
-        <OutputSection aiOutput={aiOutput} />
+    return (
+        <div>
+            <Link href='/dashboard'>
+                <Button className='m-4 flex'> <ArrowLeft className='w-5 h-5 ml-1' /> Back</Button>
+            </Link>
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5'>
+
+                {/* Form section  */}
+                <FormSection selectedTemplates={selectTemplate} userFormInput={(data: any) => GenerateAIContent(data)} loading={loading} />
+
+                {/* Output section  */}
+                <div className='col-span-2'>
+                    <OutputSection aiOutput={aiOutput} />
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Page
